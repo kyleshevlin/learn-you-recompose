f99e9466b8b62ece44befd1fd733961de0069f5c
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-08-19 19:27:24 -0700

    add prism js for highlighting

--- src/js/Entry.jsx
+++ src/js/Entry.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import 'prismjs/prism' // eslint-disable-line import/first
 import '../scss/app.scss'
+import 'prismjs/themes/prism-okaidia.css' // eslint-disable-line import/first
 
 ReactDOM.render(<App />, document.getElementById('app'))
