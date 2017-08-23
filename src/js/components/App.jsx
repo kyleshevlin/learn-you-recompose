312e102b3a245dc774b4aef620067d87c37f4c3a
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-08-22 23:15:02 -0700

    replace greeting with header

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../store'
-import Greeting from './Greeting'
+import Header from './Header'
 import All from './chapters/All'
 
 const App = () =>
   <Provider store={store}>
     <div className="wrap">
       <div className="container">
-        <Greeting message="Greetings from Learn You Recompose!" />
+        <Header />
         <All />
       </div>
     </div>
---
+++ src/js/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () =>
+  <header className="header">
+    <h1 className="header-heading">Learn You Recompose</h1>
+    <h4 className="header-subheading">
+      By <a href="https://twitter.com/kyleshevlin">@kyleshevlin</a>
+    </h4>
+  </header>
+
+export default Header
